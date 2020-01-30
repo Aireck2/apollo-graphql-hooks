20a4a9a7365dfd93886b97d5839b73b3b9950571
Erick <erickescribaa@gmail.com>
2020-01-29 21:52:49 -0500

    (#1) feat: add ui and router dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
-    <div>
-      <h1>Hi world!</h1>
-    </div>
+    <Router>
+      <MenuBar />
+      <div style={{ padding: "40px" }}>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </div>
+    </Router>
   );
 }
 
