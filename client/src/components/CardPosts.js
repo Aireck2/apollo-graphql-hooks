52d6efc09f14d01062d22f5865974926eb96b7dd
Erick <erickescribaa@gmail.com>
2020-01-30 12:32:01 -0500

    (#2) feat: improve UI for data print

--- client/src/components/CardPosts.js
+++ client/src/components/CardPosts.js
@@ -2,27 +2,34 @@ import React from "react";
 import { Card, Comment, Avatar, Tooltip, Icon } from "antd";
 import moment from "moment";
 
-const CardPost = () => {
+const CardPost = post => {
+  const { likeCount, commentCount, username, body } = post.post;
   const actions = [
-    <span key="comment-basic-like">
-      <Tooltip title="Like">
-        <Icon type="like" theme="filled" />
-      </Tooltip>
-      <span style={{ paddingLeft: 8, cursor: "auto" }}>2</span>
-    </span>
+    <React.Fragment>
+      <span key="comment-basic-like">
+        <Tooltip title="Like">
+          <Icon type="like" theme="filled" />
+        </Tooltip>
+        <span style={{ paddingLeft: 8, cursor: "auto" }}>{likeCount}</span>
+      </span>
+      <span key="comment-basic-like">
+        <Tooltip title="Like">
+          <Icon type="message" theme="filled" />
+        </Tooltip>
+        <span style={{ paddingLeft: 8, cursor: "auto" }}>{commentCount}</span>
+      </span>
+    </React.Fragment>
   ];
 
   return (
     <Card>
       <Comment
         actions={actions}
-        author="Erick"
+        author={username}
         avatar={
-          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"></Avatar>
+          <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRAJLzhe1MEiXQ6FHGoZkKEkzzTsZ5onGK1Ah0acCbKrEIXExxs"></Avatar>
         }
-        content="We supply a series of design principles, practical patterns and high quality design
-            resources (Sketch and Axure), to help people create their product prototypes beautifully
-            and efficiently"
+        content={body}
         datetime={
           <Tooltip title={moment().format("YYYY-MM-DD HH:mm:ss")}>
             <span>{moment().fromNow()}</span>
